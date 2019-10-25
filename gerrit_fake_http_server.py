802cce3f830de57d92577d8abceaeef42a0b4629
gryf <gryf73@gmail.com>
2019-10-25 21:05:13 +0200

    Added new REST endpoints.
    Two new endponds added:
    
    - /a/plugins/events-log/events/
    - /a/changes

--- gerrit_fake_http_server.py
+++ gerrit_fake_http_server.py
@@ -2,7 +2,7 @@ import bottle
 
 
 @bottle.route('/plugins/events-log/')
-def events(params=None):
+def events_log(params=None):
     return ''
 
 
@@ -29,6 +29,18 @@ def projects(params=None):
                           "web_links": [{"name": "browse",
                                          "url": "/plugins/gitiles/DEDICATED",
                                          "target": "_blank"}]}}
+
+
+@bottle.post('/a/changes')
+def changes(param=None):
+    print(param)
+    print(bottle.request.json)
+    pass
+
+
+@bottle.route('/a/plugins/events-log/events/')
+def events(t1=None):
+    return {}
 
 
 bottle.run(host='localhost', port=8181, debug=True)
