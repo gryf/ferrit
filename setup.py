57bf9556c7e902b2a2097e0cde587fc0079018ad
gryf <gryf73@gmail.com>
2019-10-28 19:13:04 +0100

    Added python package setup and tox

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+
+requirements = ['paramiko', 'bottle']
+
+
+setup(author="Roman Dobosz, Szymon Datko",
+      author_email='gryf73@gmail.com',
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, '
+                      '!=3.5.*',
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   "Programming Language :: Python :: 2",
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8'],
+      description="Ferrit is a fake gerrit server for testing purposes",
+      entry_points={'console_scripts': ['ferrit=ferrit.cli:main']},
+      install_requires=requirements,
+      license="BSD license",
+      long_description=readme,
+      include_package_data=True,
+      keywords='ferrit',
+      name='ferrit',
+      packages=find_packages(include=['ferrit', 'ferrit.*']),
+      test_suite='tests',
+      url='https://github.com/gryf/ferrit',
+      version='0.0.1',
+      zip_safe=False)
