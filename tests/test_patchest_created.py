e65a5d28a391cd3dfa8f89afe2c7d718dc2c09ac
gryf <gryf73@gmail.com>
2019-11-11 17:40:49 +0100

    Use _send_and_get_response in test_send_patch.

--- tests/test_patchest_created.py
+++ tests/test_patchest_created.py
@@ -1,8 +1,4 @@
 import json
-import os
-import time
-
-import requests
 
 from tests import base
 
@@ -10,23 +6,7 @@ from tests import base
 class TestPatchsetCreated(base.BaseTestCase):
 
     def test_send_patch(self):
-        counter = 20
-        result = None
-        requests.post('http://localhost:8181/make/event', data='project='
-                      'example&branch=master&type=patchset-created')
-        while counter:
-            if not os.path.exists('ferrit-http.log'):
-                counter -= 1
-                time.sleep(1)
-                continue
-
-            with open('ferrit-http.log') as fobj:
-                result = fobj.read()
-                if not result:
-                    time.sleep(1)
-                    counter -= 1
-                else:
-                    break
+        result = self._send_and_get_response('patchset-created')
 
         self.assertTrue(result)
         result = json.loads(result)
